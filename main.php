9f647b10be579209e75397420883eff31944d9be
ottavio-bitencourt-flores <flores.ottavio@escola.pr.gov.br>
2026-08-03 22:16:02 +0000

    hh

--- main.php
+++ main.php
@@ -1,80 +1,249 @@
- <?php
-//   class Pessoa{
-//     public $nome;
-//     private $cpf;
+<?php
 
-//     public function __construct($nome, $cpf){
-//       $this->nome = $nome;
-//       $this->cpf = $cpf;
-//     }
+// Exercício 1
 
-//     public function apresentar(){
-//       return "Nome: $this->nome\nCPF: $this->cpf\n";
-//     }
+class Usuario {
+    private string $nome;
+    private string $email;
 
-//     public function setNovoCpf($novoCpf){
-//       $this->cpf = $novoCpf;
-//       return "CPF modificado com sucesso!";
-//     }
-
-//     public function getCpf(){
-//       return $this->cpf;
-//     }
-//   }
-
-//   class Funcionario extends Pessoa{
-//     public $nome;
-//     private $cpf;
-//     protected $numReg;
-
-//     public function __construct($nome, $cpf, $numReg){
-//       parent::__construct($nome, $cpf);
-//       $this->numReg = $numReg;
-//     }
-
-//     public function apresentar(){
-//       return parent::apresentar() . "\nNúmero Registro: $this->numReg";
-//     }
-//   }
-
-//   $ana = new Pessoa("Ana", 12345678900);
-//   echo "Nome impresso diretamente: " . $ana->nome . "\n";
-//   echo "CPF impresso diretamente: " . $ana->cpf . "\n";
-//   echo "Dados impressos pelo método apresentar(): \n";
-//   echo $ana->apresentar();
-//   echo $ana->setNovoCpf(readline("Digite um novo CPF para $ana->nome: "));
-//   echo "Novo CPF: " . $ana->getCpf();
-//   $bruno = new Funcionario("Bruno", 98765432100, 12345);
-//   echo $bruno->apresentar();
-
-
- abstract class Funcionario{
-    public $nome;
-    protected $cpf;
-    private  $numReg;
-
-    public function __construct($nome, $cpf, $numReg){
-      $this->nome = $nome;
-      $this->cpf = $cpf;
-      $this->numReg = $numReg;
-      
+    public function __construct(string $nome, string $email) {
+        $this->nome = $nome;
+        $this->email = $email;
     }
- public function apresentar(){
-   return "Nome: $this->nome\nCPF: $this->cpf\nNúmero de registro: $this->numReg\n ";
- }
-   abstract public function promover($salario, $aumentoPercentual);
- }
-class RecursosHumanos extends Funcionario{
-  public function promover($salario, $aumentoPercentual){
-   return $salario + ($salario * $aumentoPercentual / 100);
-  }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void {
+        $this->nome = $nome;
+    }
+
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+
+    public function exibirDados(): void {
+        echo "Nome: " . $this->nome . PHP_EOL;
+        echo "E-mail: " . $this->email . PHP_EOL;
+    }
 }
-$ana = new RecursosHumanos("Ana", 12345678900, 12345);
-echo $ana->apresentar();
-echo $ana->promover(7500, 17);
+
+class Administrador extends Usuario {
+    private string $nivelAcesso;
+
+    public function __construct(string $nome, string $email, string $nivelAcesso) {
+        parent::__construct($nome, $email);
+        $this->nivelAcesso = $nivelAcesso;
+    }
+
+    public function getNivelAcesso(): string {
+        return $this->nivelAcesso;
+    }
+
+    public function setNivelAcesso(string $nivelAcesso): void {
+        $this->nivelAcesso = $nivelAcesso;
+    }
+
+    public function exibirDados(): void {
+        parent::exibirDados();
+        echo "Nível de Acesso: " . $this->nivelAcesso . PHP_EOL;
+    }
+}
+
+$admin = new Administrador("Ludson Moreira", "ludson@empresa.com", "Moderador");
+$admin->setNivelAcesso("Administrador Geral");
+$admin->exibirDados();
+
+echo PHP_EOL;
+
+// Exercício 2
+
+class Produto {
+    protected string $nome;
+    protected float $preco;
+
+    public function __construct(string $nome, float $preco) {
+        $this->nome = $nome;
+        $this->preco = $preco;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void {
+        $this->nome = $nome;
+    }
+
+    public function getPreco(): float {
+        return $this->preco;
+    }
+
+    public function setPreco(float $preco): void {
+        $this->preco = $preco;
+    }
+
+    public function mostrarProduto(): void {
+        echo "Produto: " . $this->nome . PHP_EOL;
+        echo "Preço: R$ " . number_format($this->preco, 2, ',', '.') . PHP_EOL;
+    }
+}
+
+class Notebook extends Produto {
+    private string $memoriaRAM;
+
+    public function __construct(string $nome, float $preco, string $memoriaRAM) {
+        parent::__construct($nome, $preco);
+        $this->memoriaRAM = $memoriaRAM;
+    }
+
+    public function getMemoriaRAM(): string {
+        return $this->memoriaRAM;
+    }
+
+    public function setMemoriaRAM(string $memoriaRAM): void {
+        $this->memoriaRAM = $memoriaRAM;
+    }
+
+    public function mostrarProduto(): void {
+        parent::mostrarProduto();
+        echo "Memória RAM: " . $this->memoriaRAM . PHP_EOL;
+    }
+}
+
+$notebook = new Notebook("Dell Inspiron", 3500.00, "8GB");
+$notebook->setMemoriaRAM("16GB");
+$notebook->mostrarProduto();
+
+echo PHP_EOL;
+
+// Exercício 3
+
+class Funcionario {
+    private string $nome;
+    private float $salario;
+
+    public function __construct(string $nome, float $salario) {
+        $this->nome = $nome;
+        $this->salario = $salario;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void {
+        $this->nome = $nome;
+    }
+
+    public function getSalario(): float {
+        return $this->salario;
+    }
+
+    public function setSalario(float $salario): void {
+        $this->salario = $salario;
+    }
+
+    public function mostrarFuncionario(): void {
+        echo "Funcionário: " . $this->nome . PHP_EOL;
+        echo "Salário: R$ " . number_format($this->salario, 2, ',', '.') . PHP_EOL;
+    }
+}
+
+class Desenvolvedor extends Funcionario {
+    private string $linguagemPrincipal;
+
+    public function __construct(string $nome, float $salario, string $linguagemPrincipal) {
+        parent::__construct($nome, $salario);
+        $this->linguagemPrincipal = $linguagemPrincipal;
+    }
+
+    public function getLinguagemPrincipal(): string {
+        return $this->linguagemPrincipal;
+    }
+
+    public function setLinguagemPrincipal(string $linguagemPrincipal): void {
+        $this->linguagemPrincipal = $linguagemPrincipal;
+    }
+
+    public function mostrarFuncionario(): void {
+        parent::mostrarFuncionario();
+        echo "Linguagem Principal: " . $this->linguagemPrincipal . PHP_EOL;
+    }
+}
+
+$dev = new Desenvolvedor("Ana Maria", 4500.00, "PHP");
+$dev->setSalario(5200.00);
+$dev->mostrarFuncionario();
+
+echo PHP_EOL;
+
+// Exercício 4
+
+class Pessoa {
+    protected string $nome;
+    protected int $idade;
+
+    public function __construct(string $nome, int $idade) {
+        $this->nome = $nome;
+        $this->idade = $idade;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void {
+        $this->nome = $nome;
+    }
+
+    public function getIdade(): int {
+        return $this->idade;
+    }
+
+    public function setIdade(int $idade): void {
+        $this->idade = $idade;
+    }
+
+    public function apresentar(): void {
+        echo "Nome: " . $this->nome . PHP_EOL;
+        echo "Idade: " . $this->idade . " anos" . PHP_EOL;
+    }
+}
+
+class Aluno extends Pessoa {
+    private string $curso;
+
+    public function __construct(string $nome, int $idade, string $curso) {
+        parent::__construct($nome, $idade);
+        $this->curso = $curso;
+    }
+
+    public function getCurso(): string {
+        return $this->curso;
+    }
+
+    public function setCurso(string $curso): void {
+        $this->curso = $curso;
+    }
+
+    public function apresentar(): void {
+        parent::apresentar();
+        echo "Curso: " . $this->curso . PHP_EOL;
+    }
+}
+
+$aluno = new Aluno("João Pedro", 16, "Desenvolvimento de Sistemas");
+$aluno->setIdade(17);
+$aluno->apresentar();
+
 
 ?>
-
   
   
   
